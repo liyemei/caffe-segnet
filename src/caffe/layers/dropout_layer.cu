794fdd54f8ed98a4f71ecf4a63f319b30c1fd4f6
Liye Mei <702131525@qq.com>
2017-08-22 19:23:32 +0800

    Update dropout_layer.cu

--- src/caffe/layers/dropout_layer.cu
+++ src/caffe/layers/dropout_layer.cu
@@ -20,28 +20,17 @@ void DropoutLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
   const Dtype* bottom_data = bottom[0]->gpu_data();
   Dtype* top_data = top[0]->mutable_gpu_data();
   const int count = bottom[0]->count();
-  if (this->phase_ == TRAIN) {
+  if (this->phase_ == TRAIN || this->layer_param_.dropout_param().sample_weights_test()) {
     unsigned int* mask =
         static_cast<unsigned int*>(rand_vec_.mutable_gpu_data());
     caffe_gpu_rng_uniform(count, mask);
     // set thresholds
-    if (scale_train_) {
-      // NOLINT_NEXT_LINE(whitespace/operators)
-      DropoutForward<Dtype><<<CAFFE_GET_BLOCKS(count),
-        CAFFE_CUDA_NUM_THREADS>>>(
-          count, bottom_data, mask, uint_thres_, scale_, top_data);
-    } else {
-      // NOLINT_NEXT_LINE(whitespace/operators)
-      DropoutForward<Dtype><<<CAFFE_GET_BLOCKS(count),
-        CAFFE_CUDA_NUM_THREADS>>>(
-          count, bottom_data, mask, uint_thres_, 1.f, top_data);
-    }
+    // NOLINT_NEXT_LINE(whitespace/operators)
+    DropoutForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+        count, bottom_data, mask, uint_thres_, scale_, top_data);
     CUDA_POST_KERNEL_CHECK;
   } else {
     caffe_copy(count, bottom_data, top_data);
-    if (!scale_train_) {
-      caffe_gpu_scal<Dtype>(count, 1. / scale_, top_data);
-    }
   }
 }
 
@@ -61,27 +50,17 @@ void DropoutLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
   if (propagate_down[0]) {
     const Dtype* top_diff = top[0]->gpu_diff();
     Dtype* bottom_diff = bottom[0]->mutable_gpu_diff();
-    if (this->phase_ == TRAIN) {
+    if (this->phase_ == TRAIN || this->layer_param_.dropout_param().sample_weights_test()) {
       const unsigned int* mask =
           static_cast<const unsigned int*>(rand_vec_.gpu_data());
       const int count = bottom[0]->count();
-      if (scale_train_) {
-        // NOLINT_NEXT_LINE(whitespace/operators)
-        DropoutBackward<Dtype><<<CAFFE_GET_BLOCKS(count),
-          CAFFE_CUDA_NUM_THREADS>>>(
-            count, top_diff, mask, uint_thres_, scale_, bottom_diff);
-      } else {
-        // NOLINT_NEXT_LINE(whitespace/operators)
-        DropoutBackward<Dtype><<<CAFFE_GET_BLOCKS(count),
-          CAFFE_CUDA_NUM_THREADS>>>(
-           count, top_diff, mask, uint_thres_, 1.f, bottom_diff);
-      }
+      // NOLINT_NEXT_LINE(whitespace/operators)
+      DropoutBackward<Dtype><<<CAFFE_GET_BLOCKS(count),
+        CAFFE_CUDA_NUM_THREADS>>>(
+          count, top_diff, mask, uint_thres_, scale_, bottom_diff);
       CUDA_POST_KERNEL_CHECK;
     } else {
       caffe_copy(top[0]->count(), top_diff, bottom_diff);
-      if (!scale_train_) {
-        caffe_gpu_scal<Dtype>(top[0]->count(), 1. / scale_, bottom_diff);
-      }
     }
   }
 }
